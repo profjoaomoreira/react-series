056159236d0f76a2e61c4accda3ef611b7014aa2
JoaoAlan <professorjoaomoreira@gmail.com>
2019-12-29 09:39:10 -0300

    final video5

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import Contador from './Contador';
+
 export default class App extends React.Component {
   render() {
-    return <h1>Olá React!</h1>;
+    return (
+    <div>
+        <Contador />
+    </div>
+
+  );
+    
   }
 }
