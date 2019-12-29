3f2961ecadd1ebbeee113c30f59cf8cfd95dca4c
JoaoAlan <professorjoaomoreira@gmail.com>
2019-12-29 08:41:23 -0300

    final video4

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './components/App';
+
 ReactDOM.render (
-  <h1>Olá Mundo!</h1>,
-document.getElementById('app')
+  <App />,
+    document.getElementById("app")
   );
